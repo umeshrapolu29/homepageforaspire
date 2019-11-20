4185ecd1d6de6631a5a209c61d5a263ff4b5bcae
umeshrapolu29@gmail.com <sampathkumar0078@gmail.com>
2019-11-20 12:48:25 +0530

    added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { FormComponent } from './form/form.component';
+import { DropComponent } from './drop/drop.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ {
+    path:'form11',component:FormComponent
+  },
+  {path:'drop',component:DropComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormComponent } from './form/form.component';
+import { DropComponent } from './drop/drop.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    FormComponent,
+
+    DropComponent
   ],
   imports: [
     BrowserModule,
